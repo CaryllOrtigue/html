e5c56abfaf54f8d809a9f28379fb1d679451794e
CaryllOrtigue <77064960+CaryllOrtigue@users.noreply.github.com>
2021-02-24 08:43:28 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         <title> homepage </title>
 </head>
         
-<body bodybackground="hope.jpg">
+<body bodybackground='hope.jpg'>
 <h1 align="center">
 <br><br><br><br><br><br><br><br>
 <b>TODAY AND TOMORROW</b>
