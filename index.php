4fa4ea54ed434c06f98842a93ef6486478f64f3b
CaryllOrtigue <77064960+CaryllOrtigue@users.noreply.github.com>
2021-02-24 10:58:26 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <html>
 <head>
         <title> homepage </title>
-<body background="today.jpg">
+<body background="dawn.jpg">
 </head>    
 <body>
 <h1 align="center">
 Today and Tomorrow
 </h1>
 <h2 align="center"><font face="verdana" size="24">
-<br><br><br><br><br><br>
+<br><br><br><br><br>
 <b>WELCOME TO OUR HOMEPAGE</b>
 </font>
 </h2>
