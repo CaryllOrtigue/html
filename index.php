8b01609c2cbb08d048c1c56d17496b5eb019b4ab
CaryllOrtigue <77064960+CaryllOrtigue@users.noreply.github.com>
2021-02-24 09:55:17 -0800

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
 <h1 align="center">
 Today and Tomorrow
 </h1>
-<h2 align="center">
+<h2 align="center"><font face="verdana size="24">
 <br><br><br><br><br><br>
-<b>WELCOME TO OUR HOMEPAGE</b>
+<b >WELCOME TO OUR HOMEPAGE</b>
 </br>
+</font>
 </h2>
 </body>
-</hmtl>
+</html>
