66dc17b86f6fd9ee19afe2c21881d459e0e90b13
CaryllOrtigue <77064960+CaryllOrtigue@users.noreply.github.com>
2021-02-28 01:36:46 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,48 @@
 <html>
 <head>
-        <title> homepage </title>
+<title>Today and Tomorrow</title>
 </head>
-<body background="dawn.jpg">
+<body background="dawn.jpg"
 <body>
 <h1 align="center">
-Today and Tomorrow
-</h1>
-<h2 align="center"><font face="verdana" size="24">
-<br><br><br><br><br>
-<b>WELCOME TO OUR HOMEPAGE</b>
-</font>
-</h2>
+TODAY AND TOMORROW
+<br>
+Our issue refers to our current situation which is all about pandemic.</h1>
+<br><br>
+<p>TODAY:</p>
+<p>We attempted to compress into a single word this pandemic that made its way into our young lives and wreaked 
+<p>havoc upon its wake.Through the lens of the young, I wonder how it feels to see the world somehow fall apart 
+<p>through our very own eyes, seeing most of humanity struggling to fight back with this unseen threat. At such a 
+<p>young and crucial point in our lives, we are experiencing a global crisis not just on health, but also on many 
+<p>aspects of our society that the extent of its damaging repercussions might be too complex and complicated for 
+<p>us to digest.</p>
+<br>
+<p>Now, as we struggle to transition from our young lives without lockdown to the ‘new normal’ with the rest of the 
+<p>world, my hope is that the spirit of our resilience and the camaraderie among Filipinos and the global community 
+<p>persist and win against all the forces that try to bring the whole world down. Dont lose hope, we will counterpart 
+<p>this pandemic.</p>
+<br>
+<p>Let us stay safe, well-informed and positive as we fight hand in hand. We will get through this crisis together and 
+<p>we will be multiple steps closer towards a new world with a healed humankind.</p>
+<br>
+<p>TOMORROW:</p>
+<img src="bird.jpg" width="400"/>
+<p>Hope a shimmer of light that gives me strength and chance</p>
+<p>It is what gets me up at a time of despair </p>
+<p>You are braver than you believe, and stronger than you seem,</p>
+<p>And smarter than you think.</p>
+<br>
+<p>First and foremost, we must inculcate into our minds to stay home, unless it is truly necessary or if there are 
+<p>immediate needs that must be acquired outside of our homes. Consequently, let us spread this message to our peers
+<p>and also those who are within the reach of our influence. In order to flatten the curve, we must participate in 
+<p>mitigating the increasing number of cases. Observe physical distance and, when you are sick, practice self-isolation 
+<p>from the others. This is already a great help to combat the pandemic. So, stay home, peers! Even through this Covid-19 
+<p>pandemic, we should look to God and not lose hope in him. We ne<p>ed to be using this as an opportunity and discover 
+<p>the godly way of facing trials and tribulations in our own individual lives and as a community. Don't give up no matter 
+<p>what cause we can surely recover we must fight for our lives. Always trust in God. Believe in His ways, remember that 
+<p>we are never alone, to those people who lose hope always remember that you have a love ones. Also the best medicine is 
+<p>to pray and trust in God.If you are thinking of giving up, just keep in mind that there is a rainbow after the rain. 
+<p>Whatever you're experiencing now is just a trial. It will soon end and we will start a new beginning. Let's just pray 
+<p>and trust Him. Good things will come soon after. Don't give up because giving up is never an option.</p>	
 </body>
 </html>
